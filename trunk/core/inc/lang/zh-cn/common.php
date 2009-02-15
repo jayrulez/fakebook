4d9ea9c065e21ed0f8d0eefb437542b560742daf
6@imdingo.com <6@imdingo.com@eb5e8b02-f981-11dd-9fdb-3debd5008f28>
2009-02-15 00:22:04 +0000

    zh-cn

--- trunk/core/inc/lang/zh-cn/common.php
+++ trunk/core/inc/lang/zh-cn/common.php
@@ -4,6 +4,15 @@ return array
 (
 	'_SITE_CLOSED_'		=>	'',
 	'_SIGNUP_CLOSED_'	=>	'',
+	
+	'link_home'	=>	'首页',
+	'link_profile'	=>	'个人主页',
+	'link_friends'	=>	'朋友',
+	'link_inbox'	=>	'收件箱',
+	'link_settings'	=>	'设置',
+	'link_signout'	=>	'注册',
+	'link_signin'	=>	'登录',
+	'link_signup'	=>	'退出',
 );
 
 ?>
