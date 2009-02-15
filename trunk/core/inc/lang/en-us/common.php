37dcb6112c42d55b9c38d589218fa155756e16fe
waprave <waprave@eb5e8b02-f981-11dd-9fdb-3debd5008f28>
2009-02-15 20:25:12 +0000

    added quicklogin form

--- trunk/core/inc/lang/en-us/common.php
+++ trunk/core/inc/lang/en-us/common.php
@@ -13,6 +13,12 @@ return array
 	'link_signout'	=>	'Sign Out',
 	'link_signin'	=>	'Sign In',
 	'link_signup'	=>	'Sign Up',
+	
+	'field_login_id'	=>	'Sign In ID:',
+	'field_login_pwd'	=>	'Password:',
+	'field_autologin'	=>	'Remember me?',
+	'quicklogin_submit'	=>	'Sign In',
+	'link_forgot_password'	=>	'Forgot Password?',
 );
 
 ?>
