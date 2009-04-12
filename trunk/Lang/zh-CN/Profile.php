bea0eeac9a9775873886d4d03413e5b2c1b5fe12
6@imdingo.com <6@imdingo.com@eb5e8b02-f981-11dd-9fdb-3debd5008f28>
2009-04-12 15:39:22 +0000

    profile: actions

--- trunk/Lang/zh-CN/Profile.php
+++ trunk/Lang/zh-CN/Profile.php
@@ -11,6 +11,14 @@ return array
 	'_friend_find'			=>	'查找您认识的人',
 
 	'_group_header'		=>	'群组',
+
+	'_action_view_photo_me'			=>	'查看其中有我的照片',
+	'_action_edit_profile'			=>	'编辑我的个人主页',
+	'_action_view_photo_someone'	=>	'查看其中有 %s 的照片',
+	'_action_suggest_friend'		=>	'为 %s 推荐朋友',
+	'_action_send_message'			=>	'给 %s 发消息',
+	'_action_poke_someone'			=>	'和 %s 打个招呼',
+	'_action_confirm_friend'		=>	'确认朋友请求',
 );
 
 ?>
