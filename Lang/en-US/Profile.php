18afd2e485d983c89d3252ac1df5b97c11d42b99
6 <6@imdingo.com>
2009-04-12 15:39:22 +0000

    profile: actions

--- Lang/en-US/Profile.php
+++ Lang/en-US/Profile.php
@@ -11,6 +11,14 @@ return array
 	'_friend_find'			=>	'Find people you know',
 
 	'_group_header'		=>	'Groups',
+
+	'_action_view_photo_me'			=>	'View Photos of me',
+	'_action_edit_profile'			=>	'Edit Profile',
+	'_action_view_photo_someone'	=>	'View Photos of %s',
+	'_action_suggest_friend'		=>	'Suggest Friends for %s',
+	'_action_send_message'			=>	'Send %s a Message',
+	'_action_poke_someone'			=>	'Poke %s',
+	'_action_confirm_friend'		=>	'Confirm Friend',
 );
 
 ?>
